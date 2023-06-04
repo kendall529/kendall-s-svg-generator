791a98320c64f027a3e3b74006392b67731b16b5
kendall529 <kendall.smith529@gmail.com>
2023-06-04 09:35:38 -0500

    working code minus a finished README

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const questions = [
         type: 'input',
         name: 'text',
         message: 'Enter 3 characters of text:',
-        validate: (input) => input.length <= 3,
+        validate: (input) => {
+            if(input.length > 3) {
+                return "Input must be no more than 3 character!"
+            }
+            return true;
+        },
     },
     {
         type: 'input',
@@ -43,31 +48,28 @@ function writeToFile(fileName, responses) {
 
     svgResult += `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`
 
-    let shapeResult, svgFragment;
+    let shapeResult;
 
     switch (responses.shape) {
         case 'Triangle':
             shapeResult = new Triangle();
-            svgFragment += `<polygon points="75, 6 244, 194 26, 194" fill="${responses.shapeColor}" />`
+            svgResult += `<polygon points="34.5299, 200 150, 0 265.4701, 200" fill="${responses.shapeColor}" />`
             break;
         case 'Circle':
             shapeResult = new Circle();
-            svgFragment += `<circle cx="150" cy="100" r="80" fill="${responses.shapeColor}" />`
+            svgResult += `<circle cx="150" cy="100" r="100" fill="${responses.shapeColor}" />`
             break;
         default:
             shapeResult = new Square();
-            svgFragment += `<rect x="70" y="20" width="160" height="160" fill="${responses.shapeColor}" />`
+            svgResult += `<rect x="50" y="0" width="200" height="200" fill="${responses.shapeColor}" />`
             break;
     }
 
-    svgResult += svgFragment;
-
-    svgResult += `<text x="150" y="125" font-size="60" text-anchor="middle" fill=${responses.textColor}>${responses.text}</text>
+    svgResult += `<text x="150" y="150" font-size="60" text-anchor="middle" fill=${responses.textColor}>${responses.text}</text>
     </svg>`
 
-    return svgResult;
-
-    fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log(`Generated logo.svg`));
+    fs.writeFile(fileName, svgResult, (err) => err ? console.log(err) : console.log(`Generated logo.svg`));
 }
+
 
 
