cfb432c3b0f7bd0d1e864f31e6bd67cd3aa1a32d
kendall529 <kendall.smith529@gmail.com>
2023-06-04 08:57:09 -0500

    changed inquirer version and updated writeToFile function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Inquirer = require('inquirer');
+const inquirer = require('inquirer');
 
 const fs = require('fs');
 
@@ -29,46 +29,45 @@ const questions = [
     },
 ]
 
-Inquirer
+inquirer
     .prompt(questions)
-    .then((response) => {
-        const {text, textColor, shape, shapeColor} = response;
-        const svgData = generateSVG(text, textColor, shape, shapeColor);
-        writeToFile('logo.svg', svgData);
+    .then((responses) => {
+        writeToFile('logo.svg', responses);
     })
     .catch((error) => {
         console.error('Error', error);
     });
 
-function writeToFile(fileName, data) {
+function writeToFile(fileName, responses) {
+    let svgResult = ''
+
+    svgResult += `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`
+
+    let shapeResult, svgFragment;
+
+    switch (responses.shape) {
+        case 'Triangle':
+            shapeResult = new Triangle();
+            svgFragment += `<polygon points="75, 6 244, 194 26, 194" fill="${responses.shapeColor}" />`
+            break;
+        case 'Circle':
+            shapeResult = new Circle();
+            svgFragment += `<circle cx="150" cy="100" r="80" fill="${responses.shapeColor}" />`
+            break;
+        default:
+            shapeResult = new Square();
+            svgFragment += `<rect x="70" y="20" width="160" height="160" fill="${responses.shapeColor}" />`
+            break;
+    }
+
+    svgResult += svgFragment;
+
+    svgResult += `<text x="150" y="125" font-size="60" text-anchor="middle" fill=${responses.textColor}>${responses.text}</text>
+    </svg>`
+
+    return svgResult;
+
     fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log(`Generated logo.svg`));
 }
 
 
-function generateSVG(text, textColor, shape, shapeColor) {
-    let shapeResult;
-
-    if(shape === 'Triangle') {
-        shapeResult = new Triangle();
-        // shapeResult = `<polygon points="75, 6 244, 194 26, 194" fill="${shapeColor}" />`
-    } else if (shape === 'Circle') {
-        shapeResult = new Circle();
-
-        // shapeResult = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`
-    } else if (shape === 'Square') {
-
-        shapeResult = new Square();
-        // shapeResult = `<rect x="70" y="20" width="160" height="160" fill="${shapeColor}" />`
-    }
-
-    const svgInput = `
-    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        
-        ${shapeResult}
-
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill=${textColor}>${text}</text>
-    </svg>
-    `;
-
-    return svgInput;
-}
